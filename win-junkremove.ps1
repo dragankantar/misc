347d246e31aa49286d0a131df6cf62188d22be4b
Dragan <dragankantar@outlook.com>
2020-11-01 14:18:23 +0800

    powershell script for removing built-in windows apps

---
+++ win-junkremove.ps1
@@ -0,0 +1,49 @@
+﻿Get-AppxPackage | Select Name, PackageFullName
+
+# template for removing any package
+Get-AppxPackage *appname* | Remove-AppxPackage
+
+# template for removing any package for all users
+Get-AppxPackage -allusers *appname* | Remove-AppxPackage
+
+# to check what apps with a given word are installed
+Get-AppxPackage *word* | Select Name
+
+# remove all the apps except for *store* and *people*
+Get-AppxPackage | where-object {$_.name –notlike "*store*"} |
+    where-object {$_.name –notlike "*people*"} | Remove-AppxPackage
+
+
+Get-AppxPackage *xbox* | Remove-AppxPackage
+Get-AppxPackage *zune* | Remove-AppxPackage
+Get-AppxPackage *maps* | Remove-AppxPackage
+Get-AppxPackage *solitaire* | Remove-AppxPackage
+Get-AppxPackage *bing* | Remove-AppxPackage
+Get-AppxPackage *phone* | Remove-AppxPackage
+Get-AppxPackage *photos* | Remove-AppxPackage
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+Get-AppxPackage *3d* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+Get-AppxPackage *feedback* | Remove-AppxPackage
+Get-AppxPackage *camera* | Remove-AppxPackage
+Get-AppxPackage *calculator* | Remove-AppxPackage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
