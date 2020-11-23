0358d41322fc4c22c50013bc11f7ee47d4dadc2c
Dragan <dragankantar@outlook.com>
2020-11-24 04:32:18 +0800

    installation of basic, basic stats, basic fin, basic ml, optimization, MDsim packages

---
+++ py-package-install.py
@@ -0,0 +1,25 @@
+conda config --add channels conda-forge
+conda config --add channels omnia
+conda config --add channels anaconda
+
+conda install numpy
+y
+conda install pandas
+y
+conda install matplotlib
+y
+conda install cvxpy
+y
+conda install statsmodels
+y
+pip install jupyterlab
+pip install cvxpylayers
+pip install yfinance
+pip install arch
+pip install scipy
+pip install tensorflow
+pip install scikit-learn
+
+conda install openmm
+conda install mdtraj
+conda install mdanalysis
